a71cb4d4250ed57bfd8c73eb100f62908d9f434d
Benjamin Klieger <bklieger@groq.com>
2024-12-03 11:19:50 -0800

    upgrade deps, start adding groq

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,71 +1,31 @@
-import { kv } from "@vercel/kv";
-import { Ratelimit } from "@upstash/ratelimit";
-import { OpenAI } from "openai";
-import {
-  OpenAIStream,
-  StreamingTextResponse,
-} from "ai";
-import { functions, runFunction } from "./functions";
+import { createGroq } from '@ai-sdk/groq';
+import { StreamingTextResponse, streamText } from 'ai';
 
-// Create an OpenAI API client (that's edge friendly!)
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+const groq = createGroq({
+  apiKey: process.env.GROQ_API_KEY,
 });
 
 export const runtime = "edge";
 
 export async function POST(req: Request) {
-  if (
-    process.env.NODE_ENV !== "development" &&
-    process.env.KV_REST_API_URL &&
-    process.env.KV_REST_API_TOKEN
-  ) {
-    const ip = req.headers.get("x-forwarded-for");
-    const ratelimit = new Ratelimit({
-      redis: kv,
-      limiter: Ratelimit.slidingWindow(50, "1 d"),
-    });
-
-    const { success, limit, reset, remaining } = await ratelimit.limit(
-      `chathn_ratelimit_${ip}`,
-    );
-
-    if (!success) {
-      return new Response("You have reached your request limit for the day.", {
-        status: 429,
-        headers: {
-          "X-RateLimit-Limit": limit.toString(),
-          "X-RateLimit-Remaining": remaining.toString(),
-          "X-RateLimit-Reset": reset.toString(),
-        },
-      });
-    }
-  }
-
   const { messages } = await req.json();
 
-  // check if the conversation requires a function call to be made
-  const initialResponse = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo-0613",
-    messages,
-    stream: true,
-    functions,
-    function_call: "auto",
+  const { textStream } = await streamText({
+    model: groq('llama-3.1-70b-versatile'),
+    messages: messages.map(message => ({
+      role: message.role,
+      content: message.content
+    }))
   });
 
-  const stream = OpenAIStream(initialResponse, {
-    experimental_onFunctionCall: async (
-      { name, arguments: args },
-      createFunctionCallMessages,
-    ) => {
-      const result = await runFunction(name, args);
-      const newMessages = createFunctionCallMessages(result);
-      return openai.chat.completions.create({
-        model: "gpt-3.5-turbo-0613",
-        stream: true,
-        messages: [...messages, ...newMessages],
-      });
-    },
+  // Create a ReadableStream from the textStream
+  const stream = new ReadableStream({
+    async start(controller) {
+      for await (const chunk of textStream) {
+        controller.enqueue(chunk);
+      }
+      controller.close();
+    }
   });
 
   return new StreamingTextResponse(stream);
